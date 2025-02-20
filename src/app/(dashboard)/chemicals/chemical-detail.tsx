4f36765d85c6c5b1ea49c05423ef120f4717066b
crthompson <crthompson@gmail.com>
2025-02-20 13:54:11 -0700

    print qr code

--- src/app/(dashboard)/chemicals/chemical-detail.tsx
+++ src/app/(dashboard)/chemicals/chemical-detail.tsx
@@ -1,4 +1,4 @@
-import { JSX, useState } from 'react';
+import { JSX, useState, useRef } from 'react';
 import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import type { Schema } from "../../../../amplify/data/resource";
@@ -16,6 +16,7 @@ import {
   Table,
   TableCell
 } from '@/components/ui/table';
+import { useReactToPrint } from "react-to-print";
 
 export function ChemicalDetail({ chemical }: { chemical: Schema["Chemicals"]["type"] }) {
 
@@ -67,30 +68,9 @@ export function ChemicalDetail({ chemical }: { chemical: Schema["Chemicals"]["ty
     }));
   };
 
-  const renderList = (list: string[]) => {
-    if(typeof list === "string") return <li>{list}</li>;
-    return list.map((element: string, index: number) => (
-      <Input key={index} type="text" value={element ?? ""} readOnly />
-    ));
-  }
-  const myLoader=({src}: { src: string })=>{
-    console.log(src);
-    return src;
-  }
-
-  function editName(name: import("@aws-amplify/data-schema").Nullable<string> | undefined): import("react").MouseEventHandler<HTMLTableCellElement> | undefined {
-    return () => {
-      const newName = prompt("Edit Chemical Name:", name ?? "");
-      console.log(newName);
-      if (newName !== null) {
-        setChemicalState((chemicalState) => ({
-          ...chemicalState,
-          name: newName,
-        }));
-      }
-    };
-  }
-
+  const contentRef = useRef<HTMLDivElement>(null);
+  const reactToPrintFn = useReactToPrint({ contentRef });
+  
   return (
     <Card>         
       <CardTitle>
@@ -99,48 +79,63 @@ export function ChemicalDetail({ chemical }: { chemical: Schema["Chemicals"]["ty
       <CardContent>
         
       <Table>
-        <TableRow>
-          <TableCell>
-            <label className="block text-sm font-medium text-gray-700">CAS</label>
-            <Input type="text" name="cas" value={chemicalState?.cas ?? ""} onChange={handleChange}/>
-          </TableCell>
-          <TableCell>
-            <label className="block text-sm font-medium text-gray-700">Marked for Disposal</label>
-            <Input type="text" name="disposal" value={chemicalState?.disposal ? "Yes": "No"} onChange={handleChange}/>
-          </TableCell>
-          <TableCell>
-            <label className="block text-sm font-medium text-gray-700">Amount</label>
-            <Input type="text" name="amount" value={chemicalState?.amount ?? ""} onChange={handleChange}/>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <label className="block text-sm font-medium text-gray-700">Classification</label>
-            <Input type="text" name="classification" value={chemicalState?.classification ?? ""} onChange={handleChange}/>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <label className="block text-sm font-medium text-gray-700">Floor</label>
-            <Input type="text" name="floor" value={chemicalState?.floor ?? ""} onChange={handleChange}/>
-          </TableCell>
-          <TableCell>
-            <label className="block text-sm font-medium text-gray-700">Area</label>
-            <Input type="text" name="area" value={chemicalState?.area ?? ""} onChange={handleChange}/>
-          </TableCell>
-          <TableCell>
-            <label className="block text-sm font-medium text-gray-700">Location Details</label>
-            <Input type="text" name="location" value={chemicalState?.location ?? ""} onChange={handleChange}/>
-          </TableCell>
-        </TableRow>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <label className="block text-sm font-medium text-gray-700">CAS</label>
+              <Input type="text" name="cas" value={chemicalState?.cas ?? ""} onChange={handleChange}/>
+            </TableCell>
+            <TableCell>
+              <label className="block text-sm font-medium text-gray-700">Marked for Disposal</label>
+              <Input type="text" name="disposal" value={chemicalState?.disposal ? "Yes": "No"} onChange={handleChange}/>
+            </TableCell>
+            <TableCell>
+              <label className="block text-sm font-medium text-gray-700">Amount</label>
+              <Input type="text" name="amount" value={chemicalState?.amount ?? ""} onChange={handleChange}/>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <label className="block text-sm font-medium text-gray-700">Classification</label>
+              <Input type="text" name="classification" value={chemicalState?.classification ?? ""} onChange={handleChange}/>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <label className="block text-sm font-medium text-gray-700">Floor</label>
+              <Input type="text" name="floor" value={chemicalState?.floor ?? ""} onChange={handleChange}/>
+            </TableCell>
+            <TableCell>
+              <label className="block text-sm font-medium text-gray-700">Area</label>
+              <Input type="text" name="area" value={chemicalState?.area ?? ""} onChange={handleChange}/>
+            </TableCell>
+            <TableCell>
+              <label className="block text-sm font-medium text-gray-700">Location Details</label>
+              <Input type="text" name="location" value={chemicalState?.location?.toString() ?? ""} onChange={handleChange}/>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>     
+              <Button onClick={handleSave}>Save</Button>
+            </TableCell>
+          </TableRow>        
+        </TableBody>
       </Table>
 
-            <div>
-            </div>
-              <QRCode value={JSON.stringify(chemicalState)} size={160} />
-            <div>
-
-            </div>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell width={100}>
+              <div ref={contentRef} style={{borderWidth: "20px", borderColor: "white"}}>
+                <QRCode value={"https://cabinet.short.gy/lvfhe9"} size={24}/>
+              </div>
+            </TableCell>
+            <TableCell>
+              <Button onClick={() => reactToPrintFn()}>Print</Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
             {/* <div>
             <label className="block text-sm font-medium text-gray-700">Location Details</label>
             <ul>{renderList(location)}</ul>
@@ -161,10 +156,6 @@ export function ChemicalDetail({ chemical }: { chemical: Schema["Chemicals"]["ty
             <label className="block text-sm font-medium text-gray-700">Course</label>
             <ul>{renderList(course)}</ul>
             </div> */}
-            
-          <div className="mt-4">
-            <Button onClick={handleSave}>Save</Button>
-          </div>
       </CardContent>
     </Card>
   );
