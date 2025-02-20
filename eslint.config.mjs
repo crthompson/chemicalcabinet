4b6915633816e95d4e8522e684fd2dc047fd7f82
crthompson <crthompson@gmail.com>
2025-02-19 17:12:29 -0700

    qr code and detail

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
-      " @typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     }
   }
 ];
